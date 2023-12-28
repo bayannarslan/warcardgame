e949f989f4b0acf25ebb8dcdacedc24351027ce8
bayannarslan <106767817+bayannarslan@users.noreply.github.com>
2023-12-28 16:16:42 -0500

    Add files via upload

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Bayann Arslan on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
